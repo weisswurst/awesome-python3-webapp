572ebf04ff85bfe7fb7e134c33d33485b20ad3f4
weisswurst <13701107906@163.com>
2020-01-31 16:32:57 +0100

    Add orm

--- root/www/orm.py
+++ root/www/orm.py
@@ -115,7 +115,11 @@ class ModelMetaclass(type):
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey
         attrs['__fields__'] = fields
-        attrs['__select__'] = 'select '
+        attrs['__select__'] = "select '%s', %s from '%s'" % (primaryKey, ','.join(escaped_fields), tableName)
+        atrrs['__insert__'] = "insert into '%s' (%s, '%S') values (%s)" % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = "update '%s' set %s where '%s'=?" % (tableName, ', '.join(map(lambda f: "'%s'=?" % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = "delete from '%s' where '%s'=?" % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
 
 
